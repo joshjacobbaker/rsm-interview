238c815eb1614db687093560b467ed0dfe1e16d0
Josh Baker <josh.jacob.baker@gmail.com>
2024-12-06 20:05:17 -0800

    Iniital Commit for Rsm Interview

---
+++ interviewPrep/keyTopics.ts
@@ -0,0 +1,12 @@
+// React.js (hook, props, etc.)
+// Material UI
+// Virtual DOM
+// High Order Components
+// Action/Reducer
+// Optimize performance in react, what strategies can we use?
+// Type of React Hooks?
+// State or Stateless?
+// How does virtual dom work in react?
+// How do we handle state management in react?
+// == v===
+// Promises, Nested Functions
