0d56c909e481e0fba4a7baf6a72ad31815462d76
Josh Baker <josh.jacob.baker@gmail.com>
2024-12-09 20:23:55 -0800

    nextjs app with ag grid

---
+++ nextjs-fetch-data-table-graph/app/page.tsx
@@ -0,0 +1,39 @@
+"use client"
+import {  useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+// React Grid Logic
+import '@ag-grid-community/styles/ag-grid.css';
+// Core CSS
+import '@ag-grid-community/styles/ag-theme-quartz.css';
+
+export default function Home() {
+  const [rowData, setRowData] = useState([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ])
+    const [colDefs, setColDefs] = useState<Record<string, string>[]>([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" }
+  ]);
+
+  const defaultColDef = {
+    flex: 1,
+};
+   
+  return (
+    // wrapping container with theme & size
+    <div
+     className="ag-theme-quartz" // applying the Data Grid theme
+     style={{ height: 500 }} // the Data Grid will fill the size of the parent container
+    >
+      <AgGridReact
+          rowData={rowData}
+          columnDefs={colDefs}
+          defaultColDef={defaultColDef}
+      />
+    </div>
+   )
+}
